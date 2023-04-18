2fe82e5a4389cc9d4f7b05e9ebd7ab01f2896c64
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-04-18 21:22:25 +0100

    Initial Commit

---
+++ ExpensesLedger/ExpensesLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesLedgerApp.swift
+//  ExpensesLedger
+//
+//  Created by Danjuma Nasiru on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
